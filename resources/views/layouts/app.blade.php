b3f7a332f85e0b0a6cc33612df2ae7fb7618b552
naoya <yamagata_7580@yahoo.co.jp>
2024-03-08 19:25:37 +0900

    ヘッダーを調整した

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.announce')
+            @include('layouts.global-header')
 
             <!-- Page Heading -->
             @if (isset($header))
---
+++ resources/views/layouts/global-header.blade.php
@@ -0,0 +1,10 @@
+<section>
+    <div class="container mx-auto flex justify-between items-center">
+        <img src="/image/logo.svg" alt="logo" class="w-20 h-20">
+        <h1 class="font-semibold text-blue-700 text-3xl mr-auto">Fit Recipe</h1>
+        <form action="">
+            <input type="text" name="keyword" class="rounded-xl border border-gray-300 px-3 py-2.5 focus:outline-none focus:ring-blue-500" placeholder='レシピを探す'>
+            <button type="submit" class="focus:outline-none text-white bg-gray-800 hover:bg-slate-500 rounded-full px-3 py-2.5 ml-2 focus:ring-4 font-medium ">検索</button>
+        </form>
+    </div>
+</section>
