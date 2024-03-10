8d842c10cd73bddf7a1aff5fab44252498fe8a54
naoya <yamagata_7580@yahoo.co.jp>
2024-03-10 22:39:24 +0900

    ボタン修正

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -7,7 +7,7 @@
             <a href="{{route('home')}}" class="text-white block text-center text-xl border border-gray-400 rounded-xl bg-gray-800 shadow-md py-2 px-3 hover:bg-slate-500 hover:text-white">ホーム画面へ戻る</a>
         </div>
         <div class="col-span-1 bg-white rounded-xl p-5 h-max sticky top-4">
-            <form name="form" method="GET" action="{{route('recipes.index')}}">
+            <form method="GET" action="{{route('recipes.index')}}">
                 <div class="flex gap-3 items-center justify-center mb-5">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
@@ -45,8 +45,8 @@
                     @endforeach
                 </div>
                 <input type="text" name="title" value="{{request()->get('title', '')}}" placeholder="お探しのレシピ名を入力してください" class="text-center border border-slate-300 p-2 rounded-xl w-full focus:ring-2">
-                <div class="flex justify-center mt-5">
-                    <button type="submit" class="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-xl mt-3 mx-auto">絞り込み検索</button>
+                <div class="flex mt-5">
+                    <button type="submit" class="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-xl mt-3 mx-auto">再検索</button>
                     <button type="button" id="clearButton" class="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-xl mt-3 mx-auto">条件クリア</button>
                 </div>
             </form>
