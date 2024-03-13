ba6e8c99cbe207a693af320dee5be24813807d60
naoya <yamagata_7580@yahoo.co.jp>
2024-03-13 13:53:49 +0900

    タイプミス修正

--- public/js/recipe/create.js
+++ public/js/recipe/create.js
@@ -5,7 +5,7 @@ window.onload = function () {
         animation: 100,
         handle: '.handle',
         onEnd: function () {
-            const items = steps.querySelectorAll('.step');
+            let items = steps.querySelectorAll('.step');
             items.forEach(function (item, index) {
                 item.querySelector('.step-number').innerHTML = '手順' + (index + 1);
             });
@@ -13,8 +13,13 @@ window.onload = function () {
     });
 
     steps.addEventListener('click', function (evt) {
-        if (evt.target.classList.contains('step-delete') || evt.target.closest('step-delete')) {
+        if (evt.target.classList.contains('.step-delete') || evt.target.closest('.step-delete')) {
             evt.target.closest('.step').remove();
+            // 削除後ソートし直す
+            let items = steps.querySelectorAll('.step');
+            items.forEach(function (item, index) {
+                item.querySelector('.step-number').innerHTML = '手順' + (index + 1);
+            });
         }
     });
 
