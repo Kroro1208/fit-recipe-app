94fdc39d6017da22103a3ea3675fde72b6574af4
naoya <yamagata_7580@yahoo.co.jp>
2024-03-27 12:57:30 +0900

    微修正

--- resources/views/layouts/announce.blade.php
+++ resources/views/layouts/announce.blade.php
@@ -4,7 +4,7 @@
     </div>
     <div class="flex mr-10 gap-3">
         @auth
-            <a href="{{route('profile.edit')}}" class="mr-10 text-xl border border-slate-400 rounded-xl bg-sky-200 py-2 px-4 shadow-lg hover:-translate-y-0.5 duration-300"">Profile</a>
+            <a href="{{route('profile.edit')}}" class="mr-10 text-xl border border-slate-400 rounded-xl bg-sky-200 py-2 px-4 shadow-lg hover:-translate-y-0.5 duration-300">Profile</a>
         @endauth
         @guest
             <a href="{{route('register')}}" class="mr-5 border border-slate-300 px-3 py-4 rounded-lg shadow-lg hover:-translate-y-0.5 duration-300">ユーザー登録（無料）</a>
